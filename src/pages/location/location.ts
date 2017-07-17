420e9bef5a7df4f70d6cd72a4a2b40a271da9745
Andre Baltieri <andrebaltieri@hotmail.com>
2017-07-17 00:16:54 -0300

    Adicionado suporte a localização

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+
 @Component({
     selector: 'page-location',
     templateUrl: 'location.html'
 })
 
 export class LocationPage {
-    constructor() {
-        navigator.geolocation.getCurrentPosition((data) => {
-            var html = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyAcVhEk1X9Gkqwnx3sAYKNbr431gS5_L_g&origin=Oslo+Norway&destination=Telemark+Norway" allowfullscreen></iframe>'
-            console.log('https://www.google.com.br/maps/@' + data.coords.latitude + '' + data.coords.longitude);
+    public location: string = '';
+    constructor(private alertCtrl: AlertController) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((data) => {
+                this.location = data.coords.latitude + ',' + data.coords.longitude;
+                var html = '<iframe style="height: 100vh;" width="100%" height="99%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyAcVhEk1X9Gkqwnx3sAYKNbr431gS5_L_g&origin=' + data.coords.latitude + ',' + data.coords.longitude + '&destination=Universidade+Anhembi+Morumbi" allowfullscreen></iframe>';
+                document.getElementById('map').innerHTML = html;
+            }, (err) => {
+                this.showLocationError(err);
+            });
+        } else {
+            this.showLocationError('');
+        }
+
+    }
+
+    showLocationError(err) {
+        let alert = this.alertCtrl.create({
+            title: 'Ops, algo deu errado',
+            subTitle: 'Não foi possível obter sua localização. ' + err.message,
+            buttons: ['OK']
         });
+        alert.present();
     }
 }
